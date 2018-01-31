c8bad529f9afd73cc1a73a8d5031874fdfc2e769
unknown <wafamuhammedali@gmail.com>
2018-01-30 22:00:45 -0800

    project is created

---
+++ WebApiDemo/WebApiDemo/Models/PersonClass.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Person
+{
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    public string Title { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    [Required]
+    [Phone]
+    public string Phone { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
